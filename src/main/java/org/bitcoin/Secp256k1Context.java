d94066e10490b02f942393cad96e0ab7970efa6c
steve preston <stevepreston747@gmail.com>
2023-09-20 14:27:52 -0700

    handle arm64 in context file

--- src/main/java/org/bitcoin/Secp256k1Context.java
+++ src/main/java/org/bitcoin/Secp256k1Context.java
@@ -32,86 +32,88 @@ import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
 /**
- * This class holds the context reference used in native methods 
+ * This class holds the context reference used in native methods
  * to handle ECDSA operations.
  */
 public class Secp256k1Context {
-  private static final boolean enabled; //true if the library is loaded
-  private static final long context; //ref to pointer to context obj
+	private static final boolean enabled; // true if the library is loaded
+	private static final long context; // ref to pointer to context obj
 
+	static { // static initializer
+		boolean isEnabled = true;
+		long contextRef = -1;
 
-  static { //static initializer
-      boolean isEnabled = true;
-      long contextRef = -1;
+		final String libToLoad;
 
-      final String libToLoad;
+		final String arch = getProperty("os.arch");
+		final boolean arch64 = "x64".equals(arch) || "amd64".equals(arch) || "x86_64".equals(arch);
+		final boolean archArm64 = "arm64".equals(arch);
 
-      final String arch = getProperty("os.arch");
-      final boolean arch64 = "x64".equals(arch) || "amd64".equals(arch)
-              || "x86_64".equals(arch);
+		final String os = getProperty("os.name");
+		final boolean linux = os.toLowerCase(ENGLISH).startsWith("linux");
+		final boolean osx = os.startsWith("Mac OS X");
+		final boolean windows = os.startsWith("Windows");
 
-      final String os = getProperty("os.name");
-      final boolean linux = os.toLowerCase(ENGLISH).startsWith("linux");
-      final boolean osx = os.startsWith("Mac OS X");
-      final boolean windows = os.startsWith("Windows");
+		try {
+			if (arch64 && linux) {
+				libToLoad = extract("coop/rchain/secp256k1-native-linux-x86_64.so");
+			} else if (arch64 && osx) {
+				libToLoad = extract("coop/rchain/secp256k1-native-osx-x86_64.dylib");
+			} else if (archArm64 && osx) {
+				libToLoad = extract("coop/rchain/secp256k1-native-osx-arm64.dylib");
+			} else if (arch64 && windows) {
+				libToLoad = extract("coop/rchain/secp256k1-native-windows-x86_64.dll");
+			} else {
+				throw new RuntimeException("No secp256k1-native library to extract");
+			}
+			System.load(libToLoad);
+			contextRef = secp256k1_init_context();
+		} catch (UnsatisfiedLinkError e) {
+			System.out.println("UnsatisfiedLinkError: " + e.toString());
+			isEnabled = false;
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.toString());
+			isEnabled = false;
+		} catch (NullPointerException e) {
+			System.out.println("Null pointer exception: " + e.toString());
+			isEnabled = false;
+		}
+		enabled = isEnabled;
+		context = contextRef;
+	}
 
-      try {
-          if (arch64 && linux) {
-              libToLoad = extract("coop/rchain/secp256k1-native-linux-x86_64.so");
-          } else if (arch64 && osx) {
-              libToLoad = extract("coop/rchain/secp256k1-native-osx-x86_64.dylib");
-          } else if (arch64 && windows) {
-              libToLoad = extract("coop/rchain/secp256k1-native-windows-x86_64.dll");
-          } else {
-              throw new RuntimeException("No secp256k1-native library to extract");
-          }
-          System.load(libToLoad);
-          contextRef = secp256k1_init_context();
-      } catch (UnsatisfiedLinkError e) {
-          System.out.println("UnsatisfiedLinkError: " + e.toString());
-          isEnabled = false;
-      } catch (IOException e){
-          System.out.println("IOException: " + e.toString());
-          isEnabled = false;
-      } catch (NullPointerException e) {
-          System.out.println("Null pointer exception: " + e.toString());
-          isEnabled = false;
-      }
-      enabled = isEnabled;
-      context = contextRef;
-  }
+	@SuppressWarnings("PMD.AssignmentInOperand")
+	private static String extract(final String name) throws IOException {
+		final String suffix = name.substring(name.lastIndexOf('.'));
+		final File file;
+		try {
+			file = createTempFile("secp256k1-native-library-", suffix);
+			file.deleteOnExit();
+			final ClassLoader cl = currentThread().getContextClassLoader();
+			try (InputStream in = cl.getResourceAsStream(name);
+					OutputStream out = Files.newOutputStream(file.toPath())) {
+				requireNonNull(in, "Classpath resource not found");
+				int bytes;
+				final byte[] buffer = new byte[4_096];
+				while (-1 != (bytes = in.read(buffer))) {
+					out.write(buffer, 0, bytes);
+				}
+			}
+			return file.getAbsolutePath();
+		} catch (final IOException e) {
+			throw new IOException("Failed to extract " + name, e);
+		}
+	}
 
-    @SuppressWarnings("PMD.AssignmentInOperand")
-    private static String extract(final String name) throws IOException {
-        final String suffix = name.substring(name.lastIndexOf('.'));
-        final File file;
-        try {
-            file = createTempFile("secp256k1-native-library-", suffix);
-            file.deleteOnExit();
-            final ClassLoader cl = currentThread().getContextClassLoader();
-            try (InputStream in = cl.getResourceAsStream(name);
-                 OutputStream out = Files.newOutputStream(file.toPath())) {
-                requireNonNull(in, "Classpath resource not found");
-                int bytes;
-                final byte[] buffer = new byte[4_096];
-                while (-1 != (bytes = in.read(buffer))) {
-                    out.write(buffer, 0, bytes);
-                }
-            }
-            return file.getAbsolutePath();
-        } catch (final IOException e) {
-            throw new IOException("Failed to extract " + name, e);
-        }
-    }
+	public static boolean isEnabled() {
+		return enabled;
+	}
 
-  public static boolean isEnabled() {
-     return enabled;
-  }
+	public static long getContext() {
+		if (!enabled)
+			return -1; // sanity check
+		return context;
+	}
 
-  public static long getContext() {
-     if(!enabled) return -1; //sanity check
-     return context;
-  }
-
-  private static native long secp256k1_init_context();
+	private static native long secp256k1_init_context();
 }
